0a664c7bc864014a2a02f033cc24dd480234723c
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-20 20:31:49 +0600

    Task 3 uploaded

---
+++ task-03.js
@@ -0,0 +1,9 @@
+// Task 3 — Countdown with While Loop
+// Write a while loop that prints numbers from 10 down to 1.
+
+let countDown = 10;
+
+while (countDown >= 1) {
+  console.log("Tik Tik", countDown);
+  countDown--;
+}
