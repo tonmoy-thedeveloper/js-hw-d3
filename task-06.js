7c0057c5651b944c8df8c2cac8c32e520f072ff8
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-20 20:46:07 +0600

    Task 6: Sum of numbers. From 1 to 20

---
+++ task-06.js
@@ -0,0 +1,8 @@
+// Task 6 — Sum of Numbers
+// Write a for loop that calculates the sum of numbers from 1 to 20 and prints the result.
+
+let sum = 0;
+for (let i = 1; i <= 20; i++) {
+  sum += i;
+}
+console.log("The total is:", sum);
